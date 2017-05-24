fa0971a1f3bb992b120318d3941bc6ecabc46862
Edwin Chen <edwinchen85@gmail.com>
2017-05-24 23:14:40 +0800

    Add or remove 'has-value' class depending on input has empty value or otherwise

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ $(function(){
 
   $('.awesome-form .input-group input').focusout(function(){
 
+    var text_val = $(this).val();
+
+    if (text_val === "") {
+      $(this).removeClass('has-value');
+    } else {
+      $(this).addClass('has-value');
+    }
+
   });
-  
+
 });
